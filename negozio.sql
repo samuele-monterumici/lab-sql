e259e4b5a6cc810ad4102b5b54de0849ca669355
samuele-monterumici <samuele.monterumici@gmail.com>
2026-03-26 09:44:23 +0100

    Update negozio.sql

--- negozio.sql
+++ negozio.sql
@@ -1,53 +1,77 @@
 --1 Estrazione di tutti i dati relativi ai modelli dei prodotti a catalogo.
-  SELECT * from 4CTL_modelli_prodotto;
+  SELECT * FROM 4CTL_modelli_prodotto;
 
 --2 Visualizzazione di nome e prezzo di tutti i modelli a catalogo.
-  SELECT nome, prezzo_listino from 4CTL_modelli_prodotto;
+  SELECT nome, prezzo_listino FROM 4CTL_modelli_prodotto;
 
 --3 Ricerca di tutti i modelli con prezzo di listino inferiore a 600€.
-  SELECT * from 4CTL_modelli_prodotto where prezzo_listino < 600
+  SELECT * FROM 4CTL_modelli_prodotto WHERE prezzo_listino < 600
 
 --4 Selezione dei modelli che contengono la stringa ’Samsung’ nel nome.
-  SELECT * from 4CTL_modelli_prodotto where nome LIKE 'Samsung%'
+  SELECT * FROM 4CTL_modelli_prodotto WHERE nome LIKE 'Samsung%'
 
 --5 Elenco dei clienti il cui nome termina con la lettera ’a’.
-  SELECT * from 4CTL_clienti where nome LIKE '%a';
+  SELECT * FROM 4CTL_clienti WHERE nome LIKE '%a';
 
 --6 Elenco dei modelli ordinati dal prezzo più elevato al più basso.
-  SELECT * from 4CTL_modelli_prodotto order by prezzo_listino desc;
+  SELECT * FROM 4CTL_modelli_prodotto ORDER BY prezzo_listino DESC;
 
 --7 Visualizzazione delle categorie di prodotti disponibili senza ripetizioni.
-  SELECT DISTINCT categoria from 4CTL_modelli_prodotto;
+  SELECT DISTINCT categoria FROM 4CTL_modelli_prodotto;
 
 --8 Estrazione dei primi 10 prodotti arrivati in magazzino in ordine cronologico.
-  SELECT * from 4CTL_prodotti order by data_arrivo LIMIT 10
+  SELECT * FROM 4CTL_prodotti ORDER BY data_arrivo LIMIT 10
 
 --9 Elenco dei clienti ordinati alfabeticamente per cognome e successivamente per nome.
-  SELECT * from 4CTL_clienti order by cognome, nome;
+  SELECT * FROM 4CTL_clienti ORDER BY cognome, nome;
 
 --10 Ricerca di tutti gli ordini effettuati nel mese di giugno 2024.
-  SELECT * from 4CTL_ordini where data_ordine LIKE '2024-06%';
+  SELECT * FROM 4CTL_ordini WHERE data_ordine LIKE '2024-06%';
 
 --11 Elenco dei codici seriali in magazzino associati al nome del relativo modello.
-  SELECT cod_seriale, nome from 4CTL_prodotti, 4CTL_modelli_prodotto where 4CTL_prodotti.id_modello=4CTL_modelli_prodotto.id_modello;
+  SELECT cod_seriale, nome 
+  FROM 4CTL_prodotti, 4CTL_modelli_prodotto 
+  WHERE 4CTL_prodotti.id_modello=4CTL_modelli_prodotto.id_modello;
 
---12 Ricostruzione dello scontrino: ID ordine, cognome cliente, nome
-modello e seriale venduto.
---13 Visualizzazione dello stato della garanzia per ogni codice seriale
-venduto.
---14 Conteggio dei pezzi fisici disponibili in magazzino per ogni categoria di
-prodotto.
---15 Calcolo del totale incassato per ogni cliente basato sulla somma degli
-ordini effettuati.
---16 Determinazione del prezzo medio di listino per ogni categoria di
-prodotto a catalogo.
---17 Identificazione del numero di prodotti venduti per ogni tipologia di
-garanzia (attiva, scaduta, in assistenza).
---18 Elenco dei clienti che hanno effettuato una spesa complessiva
-superiore a 2000€.
---19 Identificazione delle categorie che hanno più di 50 prodotti registrati a
-catalogo.
---20 Visualizzazione degli ordini il cui prezzo totale è superiore alla media di
-tutti gli ordini.
---21 Elenco dei modelli che non sono mai stati venduti (assenti nella
-tabella dettagli_ordine).
+--12 Ricostruzione dello scontrino: ID ordine, cognome cliente, nome modello e seriale venduto.
+  SELECT 4CTL_ordini.id_ordine, 4CTL_clienti.cognome, 4CTL_modelli_prodotto.nome, 4CTL_prodotti.cod_seriale 
+  FROM 4CTL_ordini, 4CTL_clienti, 4CTL_modelli_prodotto, 4CTL_prodotti, 4CTL_dettagli_ordine 
+  WHERE 1=1 
+  AND 4CTL_ordini.id_ordine=4CTL_dettagli_ordine.id_ordine 
+  AND 4CTL_dettagli_ordine.id_prodotto=4CTL_prodotti.id_prodotto 
+  AND 4CTL_prodotti.id_modello=4CTL_modelli_prodotto.id_modello 
+  AND 4CTL_clienti.id_cliente=4CTL_ordini.id_cliente
+  
+--13 Visualizzazione dello stato della garanzia per ogni codice seriale venduto.
+  SELECT stato_garanzia, cod_seriale 
+  FROM 4CTL_garanzie, 4CTL_prodotti 
+  WHERE 4CTL_garanzie.id_prodotto=4CTL_prodotti.id_prodotto
+    
+--14 Conteggio dei pezzi fisici disponibili in magazzino per ogni categoria di prodotto.
+  SELECT categoria, 
+  COUNT(id_prodotto) AS prodotti 
+  FROM 4CTL_modelli_prodotto, 4CTL_prodotti 
+  WHERE 4CTL_prodotti.id_modello=4CTL_modelli_prodotto.id_modello 
+  AND 4CTL_prodotti.disponibilita='S' 
+  GROUP BY categoria
+
+  SELECT categoria, 
+  COUNT(id_prodotto) AS prodotti 
+  FROM 4CTL_modelli_prodotto 
+  JOIN 4CTL_prodotti ON 4CTL_prodotti.id_modello=4CTL_modelli_prodotto.id_modello 
+  WHERE 4CTL_prodotti.disponibilita='S' 
+  GROUP BY categoria
+    
+--15 Calcolo del totale incassato per ogni cliente basato sulla somma degliordini effettuati.
+
+--16 Determinazione del prezzo medio di listino per ogni categoria di prodotto a catalogo.
+    
+--17 Identificazione del numero di prodotti venduti per ogni tipologia di garanzia (attiva, scaduta, in assistenza).
+    
+--18 Elenco dei clienti che hanno effettuato una spesa complessiva superiore a 2000€.
+    
+--19 Identificazione delle categorie che hanno più di 50 prodotti registrati a catalogo.
+    
+--20 Visualizzazione degli ordini il cui prezzo totale è superiore alla media di tutti gli ordini.
+    
+--21 Elenco dei modelli che non sono mai stati venduti (assenti nella tabella dettagli_ordine).
